a87d19073efb2af30a332b500b94722d9c7f7573
i2tsuki <mac-daisuki@live.jp>
2023-02-10 06:02:45 +0900

    Parse RSS Feed which getting from the internet

--- add_content/main.py
+++ add_content/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+from lxml import etree
+
+from datetime import datetime
 import json
 import requests
+import sys
 
 
 # Target class represents the media lists that this script scrapes.
@@ -13,7 +17,27 @@ class Target:
             self.rss = json.load(f)
 
 
+class Item:
+    title = ""
+    link = ""
+    description = ""
+    guid = ""
+    pubdate = datetime.fromtimestamp(0)
+
+    def __init__(self, item):
+        pass
+
+
 if __name__ == "__main__":
     target = Target()
     for url in target.rss.values():
-        r = requests.get(url=url)
+        rssitems = []
+        resp = requests.get(url=url)
+        tree = etree.fromstring(resp.content)
+        for channel in tree:
+            if channel.tag == "channel":
+                items = filter(lambda x: (x.tag == "item"), channel)
+                for item in items:
+                    rssitems.append(Item(item))
+            else:
+                sys.stderr.write("invalid rss format\n")
