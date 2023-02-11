bdadefa500063db6ae85477ae31f842f80031340
i2tsuki <mac-daisuki@live.jp>
2023-02-12 05:32:55 +0900

    Move Item class under the rss module

--- add_content/main.py
+++ add_content/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+from rss import Item
+
 from lxml import etree
 
-from datetime import datetime
 import json
 import requests
 import sys
@@ -15,21 +16,6 @@ class Target:
     def __init__(self, file="target.json"):
         with open(file=file, mode="r") as f:
             self.rss = json.load(f)
-
-
-class Item:
-    title = ""
-    link = ""
-    description = ""
-    guid = ""
-    pubdate = datetime.fromtimestamp(0)
-
-    def __init__(self, item):
-        self.title = list(filter(lambda x: (x.tag == "title"), item))[0].text
-        self.link = list(filter(lambda x: (x.tag == "link"), item))[0].text
-        self.description = list(filter(lambda x: (x.tag == "description"), item))[0].text
-        self.guid = list(filter(lambda x: (x.tag == "description"), item))[0].text
-        self.pubdate = list(filter(lambda x: (x.tag == "pubDate"), item))[0].text
 
 
 if __name__ == "__main__":
---
+++ add_content/rss/feed.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+
+class Item:
+    title = ""
+    link = ""
+    description = ""
+    guid = ""
+    pubdate = datetime.fromtimestamp(0)
+
+    def __init__(self, item):
+        self.title = list(filter(lambda x: (x.tag == "title"), item))[0].text
+        self.link = list(filter(lambda x: (x.tag == "link"), item))[0].text
+        self.description = list(filter(lambda x: (x.tag == "description"), item))[
+            0
+        ].text
+        self.guid = list(filter(lambda x: (x.tag == "description"), item))[0].text
+        self.pubdate = list(filter(lambda x: (x.tag == "pubDate"), item))[0].text
