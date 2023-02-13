be0d77d8b42f15a0b8622011fb18673805394165
i2tsuki <mac-daisuki@live.jp>
2023-02-14 06:24:12 +0900

    Add support for the media tag

--- add_content/rss/feed.py
+++ add_content/rss/feed.py
@@ -9,8 +9,9 @@ class Item:
     description = ""
     guid = ""
     pubdate = datetime.fromtimestamp(0)
+    media = None
 
-    def __init__(self, item):
+    def __init__(self, item, mediatag=""):
         self.title = list(filter(lambda x: (x.tag == "title"), item))[0].text
         self.link = list(filter(lambda x: (x.tag == "link"), item))[0].text
         self.description = list(filter(lambda x: (x.tag == "description"), item))[
@@ -20,4 +21,19 @@ class Item:
         self.pubdate = datetime.strptime(
             list(filter(lambda x: (x.tag == "pubDate"), item))[0].text,
             "%a, %d %b %Y %H:%M:%S %z",
-        )
+        )   
+        media = list(filter(lambda x: (x.tag == mediatag), item))
+        self.media = Media(media)        
+
+
+class Media:
+    contenturl = ""
+    contenttype = ""
+    contentmedium = ""
+    descriptiontype = ""
+    copyright = ""
+
+    def __init__(self, media):
+        self.contenturl = media.get(key="url")
+        self.contenttype = media.get(key="type")
+        self.contentmedium = media.get(key="medium")
