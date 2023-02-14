bf8e30fc29db79557e4f6404fdbb262dbc9b2065
i2tsuki <mac-daisuki@live.jp>
2023-02-15 06:36:36 +0900

    Handle the case for when the element is empty

--- add_content/rss/feed.py
+++ add_content/rss/feed.py
@@ -12,18 +12,15 @@ class Item:
     media = None
 
     def __init__(self, item, mediatag=""):
-        self.title = list(filter(lambda x: (x.tag == "title"), item))[0].text
-        self.link = list(filter(lambda x: (x.tag == "link"), item))[0].text
-        self.description = list(filter(lambda x: (x.tag == "description"), item))[
-            0
-        ].text
-        self.guid = list(filter(lambda x: (x.tag == "description"), item))[0].text
+        self.title = "" if (x := list(filter(lambda x: (x.tag == "title"), item))) == [] else x[0].text  # fmt: skip
+        self.link = "" if (x := list(filter(lambda x: (x.tag == "link"), item))) == [] else x[0].text  # fmt: skip
+        self.description = "" if (x := list(filter(lambda x: (x.tag == "description"), item))) == [] else x[0].text  # fmt: skip
+        self.guid = "" if (x := list(filter(lambda x: (x.tag == "guid"), item))) == [] else x[0].text  # fmt: skip
         self.pubdate = datetime.strptime(
             list(filter(lambda x: (x.tag == "pubDate"), item))[0].text,
             "%a, %d %b %Y %H:%M:%S %z",
-        )   
-        media = list(filter(lambda x: (x.tag == mediatag), item))
-        self.media = Media(media)        
+        )
+        self.media = None if (x := list(filter(lambda x: (x.tag == mediatag), item))) == [] else Media(x[0])  # fmt: skip
 
 
 class Media:
