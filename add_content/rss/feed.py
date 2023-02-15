bb52fcb9a394526bf97ec110b2bfe5c0d8ea8b70
i2tsuki <mac-daisuki@live.jp>
2023-02-16 05:47:54 +0900

    Add typehint to `feed.py`

--- add_content/rss/feed.py
+++ add_content/rss/feed.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
-
-
-class Item:
-    title = ""
-    link = ""
-    description = ""
-    guid = ""
-    pubdate = datetime.fromtimestamp(0)
-    media = None
-
-    def __init__(self, item, mediatag=""):
-        self.title = "" if (x := list(filter(lambda x: (x.tag == "title"), item))) == [] else x[0].text  # fmt: skip
-        self.link = "" if (x := list(filter(lambda x: (x.tag == "link"), item))) == [] else x[0].text  # fmt: skip
-        self.description = "" if (x := list(filter(lambda x: (x.tag == "description"), item))) == [] else x[0].text  # fmt: skip
-        self.guid = "" if (x := list(filter(lambda x: (x.tag == "guid"), item))) == [] else x[0].text  # fmt: skip
-        self.pubdate = datetime.strptime(
-            list(filter(lambda x: (x.tag == "pubDate"), item))[0].text,
-            "%a, %d %b %Y %H:%M:%S %z",
-        )
-        self.media = None if (x := list(filter(lambda x: (x.tag == mediatag), item))) == [] else Media(x[0])  # fmt: skip
+from typing import Optional
 
 
 class Media:
-    contenturl = ""
-    contenttype = ""
-    contentmedium = ""
-    descriptiontype = ""
-    copyright = ""
+    contenturl: str = ""
+    contenttype: str = ""
+    contentmedium: str = ""
+    descriptiontype: str = ""
+    copyright: str = ""
 
     def __init__(self, media):
-        self.contenturl = media.get(key="url")
-        self.contenttype = media.get(key="type")
-        self.contentmedium = media.get(key="medium")
+        self.contenturl: str = media.get(key="url")
+        self.contenttype: str = media.get(key="type")
+        self.contentmedium: str = media.get(key="medium")
+
+
+class Item:
+    title: str = ""
+    link: str = ""
+    description: str = ""
+    guid: str = ""
+    pubdate: datetime = datetime.fromtimestamp(0)
+    media: Optional[Media] = None
+
+    def __init__(self, item, mediatag=""):
+        self.title: str = "" if (x := list(filter(lambda x: (x.tag == "title"), item))) == [] else x[0].text  # fmt: skip
+        self.link: str = "" if (x := list(filter(lambda x: (x.tag == "link"), item))) == [] else x[0].text  # fmt: skip
+        self.description: str = "" if (x := list(filter(lambda x: (x.tag == "description"), item))) == [] else x[0].text  # fmt: skip
+        self.guid: str = "" if (x := list(filter(lambda x: (x.tag == "guid"), item))) == [] else x[0].text  # fmt: skip
+        self.pubdate: datetime = datetime.strptime(
+            list(filter(lambda x: (x.tag == "pubDate"), item))[0].text,
+            "%a, %d %b %Y %H:%M:%S %z",
+        )
+        self.media: Optional[Media] = None if (x := list(filter(lambda x: (x.tag == mediatag), item))) == [] else Media(x[0])  # fmt: skip
