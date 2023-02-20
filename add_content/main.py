8182dc33adb454956857fe2a9ec25c98746e8da9
i2tsuki <mac-daisuki@live.jp>
2023-02-21 05:49:15 +0900

    Store the record whether the item was read or not

--- add_content/main.py
+++ add_content/main.py
@@ -4,7 +4,7 @@ from rss import Item
 
 from lxml import etree
 
-from typing import Dict, List, Iterator, Iterable
+from typing import Any, Dict, List, Iterator, Iterable
 from datetime import date, datetime, timedelta
 import argparse
 import json
@@ -21,15 +21,25 @@ class Target:
             self.rss = json.load(f)
 
 
-def print_rss_items(tree: Iterable[etree._Element], ago=3):
+def print_rss_items(
+    tree: Iterable[etree._Element] = None, marker: Dict[str, Any] = {}, ago: int = 3
+) -> Dict[str, Any]:
     for channel in tree:
         if channel.tag == "channel":
+            if (
+                title_element := list(filter(lambda x: (x.tag == "title"), channel))
+            ) != []:
+                title: str = title_element[0].text
+            else:
+                sys.stderr.write("invalid rss format\n")
+                sys.exit(1)
             items: Iterator = filter(lambda x: (x.tag == "item"), channel)
             for i in items:
                 rssitems.append(Item(i))
         else:
             sys.stderr.write("invalid rss format\n")
             sys.exit(1)
+
     for item in rssitems:
         now: datetime = datetime.combine(date.today(), datetime.min.time()) - timedelta(
             days=ago
@@ -37,6 +47,11 @@ def print_rss_items(tree: Iterable[etree._Element], ago=3):
         if item.pubdate.timestamp() > now.timestamp():
             print(f"### [{item.title}]({item.link})")
             print(item.description)
+            if title not in marker:
+                marker[title] = {}
+            marker[title][item.title] = {"checked": True}
+
+    return marker
 
 
 if __name__ == "__main__":
@@ -68,6 +83,8 @@ if __name__ == "__main__":
     args: argparse.Namespace = parser.parse_args()
     args.from_days: int = int(args.from_days[0])
 
+    marker = {}
+
     target = Target()
     if args.select[0] == "":
         for feed in target.rss:
@@ -75,10 +92,13 @@ if __name__ == "__main__":
             rssitems: List[Item] = []
             resp: requests.Response = requests.get(url=url)
             tree: Iterable[etree._Element] = etree.fromstring(resp.content)
-            print_rss_items(tree=tree, ago=args.from_days)
+            print_rss_items(tree=tree, marker=marker, ago=args.from_days)
     else:
         url: str = target.rss[args.select[0]]
         rssitems: List[Item] = []
         resp: requests.Response = requests.get(url=url)
         tree: Iterable[etree._Element] = etree.fromstring(resp.content)
-        print_rss_items(tree=tree, ago=args.from_days)
+        print_rss_items(tree=tree, marker=marker, ago=args.from_days)
+
+    with open(file="marker.json", mode="w") as f:
+        json.dump(obj=marker, fp=f)
