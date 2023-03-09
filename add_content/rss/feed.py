48099ff8a61696b1be1d0729fe276f5abdbedb41
i2tsuki <mac-daisuki@live.jp>
2023-03-10 06:12:21 +0900

    Mark noqa for flake8

--- add_content/rss/feed.py
+++ add_content/rss/feed.py
@@ -26,12 +26,16 @@ class Item:
     media: Optional[Media] = None
 
     def __init__(self, item, mediatag=""):
-        self.title = "" if (x := list(filter(lambda x: (x.tag == "title"), item))) == [] else x[0].text  # fmt: skip
-        self.link = "" if (x := list(filter(lambda x: (x.tag == "link"), item))) == [] else x[0].text  # fmt: skip
-        self.description = "" if (x := list(filter(lambda x: (x.tag == "description"), item))) == [] else x[0].text  # fmt: skip
-        self.guid = "" if (x := list(filter(lambda x: (x.tag == "guid"), item))) == [] else x[0].text  # fmt: skip
+        # fmt: off
+        self.title = "" if (x := list(filter(lambda x: (x.tag == "title"), item))) == [] else x[0].text  # noqa
+        self.link = "" if (x := list(filter(lambda x: (x.tag == "link"), item))) == [] else x[0].text  # noqa
+        self.description = "" if (x := list(filter(lambda x: (x.tag == "description"), item))) == [] else x[0].text  # noqa
+        self.guid = "" if (x := list(filter(lambda x: (x.tag == "guid"), item))) == [] else x[0].text  # noqa
+        # fmt: on
         self.pubdate = datetime.strptime(
             list(filter(lambda x: (x.tag == "pubDate"), item))[0].text,
             "%a, %d %b %Y %H:%M:%S %z",
         )
-        self.media = None if (x := list(filter(lambda x: (x.tag == mediatag), item))) == [] else Media(x[0])  # fmt: skip
+        self.media = (
+            None if (x := list(filter(lambda x: (x.tag == mediatag), item))) == [] else Media(x[0])
+        )
