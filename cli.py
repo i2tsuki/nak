3b2ddcb22987f86957fe3392c80cf219b17636c4
i2tsuki <mac-daisuki@live.jp>
2023-03-25 06:39:01 +0900

    Support the `--debug` option

--- cli.py
+++ cli.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 import click
+from pytion import setup_logging
+
 from add import add
 
 
 @click.group()
-def cli():
-    pass
+@click.option(
+    "--debug/--no-debug", is_flag=True, default=False, help="Output verbose logs for debugging."
+)
+def cli(debug):
+    if debug:
+        setup_logging(level="debug", to_console=True, filename="pytion.log")
 
 
 cli.add_command(add)
